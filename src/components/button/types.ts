5e13aa69480c78bf2603ec6a59a721bee28ba679
Eirik Måseidvåg <eirik.maseidvag@adventuretech.no>
2023-09-15 14:06:25 +0200

    Note component (#10)

--- src/components/button/types.ts
+++ src/components/button/types.ts
@@ -1,14 +1,14 @@
 export enum ButtonThemes {
-    PRIMARY = 'primary',
-    SECONDARY = 'secondary',
-    TERTIARY = 'tertiary',
-  }
-  
-  export enum ButtonSizes {
-    SMALL = 'small',
-    MEDIUM = 'medium',
-    LARGE = 'large',
-  }
-  
-  export type IButtonThemes = `${ButtonThemes}`;
-  export type IButtonSizes = `${ButtonSizes}`;
+  PRIMARY = 'primary',
+  SECONDARY = 'secondary',
+  TERTIARY = 'tertiary',
+}
+
+export enum ButtonSizes {
+  SMALL = 'small',
+  MEDIUM = 'medium',
+  LARGE = 'large',
+}
+
+export type IButtonThemes = `${ButtonThemes}`;
+export type IButtonSizes = `${ButtonSizes}`;
