50a4e90aa8f35182542befe96f6a3d7e45052209
Daniel Larssen <21258677+danlar9@users.noreply.github.com>
2023-09-15 11:01:07 +0200

    add routes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue';
-import './style.scss';
-import App from '@/App.vue';
+import { createApp } from 'vue'
+import './style.scss'
+import App from '@/App.vue'
+import router from './plugins/router.ts';
 
-createApp(App).mount('#app');
+createApp(App).use(router).mount('#app');
