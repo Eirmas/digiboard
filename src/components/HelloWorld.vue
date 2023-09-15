8c3743cd48fc7f1ac20cb7a7aeb047f64e7e29dd
Eirik Måseidvåg <eirik.maseidvag@adventuretech.no>
2023-09-15 11:26:11 +0200

    lint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import {ref} from 'vue';
-import {BeakerIcon} from '@heroicons/vue/24/solid';
+import { ref } from 'vue';
+import { BeakerIcon } from '@heroicons/vue/24/solid';
 
 defineProps<{ msg: string }>();
 
@@ -24,8 +24,7 @@ const count = ref(0);
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
-    starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite starter
   </p>
   <p>
     Install
