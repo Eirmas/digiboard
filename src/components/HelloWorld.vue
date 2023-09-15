13b1a797d3f738180332f22ea2eead5beaa5672a
THOMAS HATLEBREKKE <t.hatlebrekke@gmail.com>
2023-09-15 10:53:00 +0200

    feat: init client

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,32 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import { supabase } from '../client';
 
 defineProps<{ msg: string }>()
 
 const count = ref(0)
+
+const onClick = () => {
+  count.value++
+};
+
+const doubleCount = computed(() => count.value * 2);
+
+const test = false
+
+const { data, error } = await supabase
+  .from('note')
+  .select();
+
+console.log(data, error);
+
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" :disabled="test" @click="onClick">doubleCount is {{ doubleCount }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
---
+++ src/database.types.ts
@@ -0,0 +1,150 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      board: {
+        Row: {
+          id: number
+          name: string
+          owner_id: number | null
+          timer_id: number | null
+          timestamp: string | null
+        }
+        Insert: {
+          id?: number
+          name: string
+          owner_id?: number | null
+          timer_id?: number | null
+          timestamp?: string | null
+        }
+        Update: {
+          id?: number
+          name?: string
+          owner_id?: number | null
+          timer_id?: number | null
+          timestamp?: string | null
+        }
+        Relationships: []
+      }
+      category: {
+        Row: {
+          board_id: number
+          id: number
+          name: string | null
+          timestamp: string | null
+        }
+        Insert: {
+          board_id: number
+          id?: number
+          name?: string | null
+          timestamp?: string | null
+        }
+        Update: {
+          board_id?: number
+          id?: number
+          name?: string | null
+          timestamp?: string | null
+        }
+        Relationships: []
+      }
+      note: {
+        Row: {
+          category_id: number | null
+          created_at: string
+          id: number
+          text: string | null
+          type: string | null
+          visible: boolean | null
+        }
+        Insert: {
+          category_id?: number | null
+          created_at?: string
+          id?: number
+          text?: string | null
+          type?: string | null
+          visible?: boolean | null
+        }
+        Update: {
+          category_id?: number | null
+          created_at?: string
+          id?: number
+          text?: string | null
+          type?: string | null
+          visible?: boolean | null
+        }
+        Relationships: []
+      }
+      note_relation: {
+        Row: {
+          action_id: number
+          created_at: string
+          note_id: number
+        }
+        Insert: {
+          action_id: number
+          created_at?: string
+          note_id: number
+        }
+        Update: {
+          action_id?: number
+          created_at?: string
+          note_id?: number
+        }
+        Relationships: [
+          {
+            foreignKeyName: "note_relation_action_id_fkey"
+            columns: ["action_id"]
+            referencedRelation: "note"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "note_relation_note_id_fkey"
+            columns: ["note_id"]
+            referencedRelation: "note"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      timer: {
+        Row: {
+          id: number
+          seconds: number
+          started_timestamp: string | null
+          visible: boolean | null
+        }
+        Insert: {
+          id?: number
+          seconds: number
+          started_timestamp?: string | null
+          visible?: boolean | null
+        }
+        Update: {
+          id?: number
+          seconds?: number
+          started_timestamp?: string | null
+          visible?: boolean | null
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
